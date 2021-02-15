9b9e2f2e7a6b08aba38f8451912eef113af5146d
Nikhilesh Yadav <nikhileshofficial@gmail.com>
2021-02-16 05:27:20 +0530

    +added description comments

--- choose.js
+++ choose.js
@@ -1,3 +1,8 @@
+/**
+ *  This module allows user to enter comapnay name,
+ *   and if the entered company name is not very specific,
+ *  it gives option to choose a more specific company name.
+ */
 const inquirer= require('inquirer');
 module.exports=async function load_company(page)
 {
@@ -6,7 +11,7 @@ module.exports=async function load_company(page)
 	{
 		var {company_name}=await inquirer.prompt([{type:'input',name:'company_name',message:'Enter name of Company :'}]);
 	        await page.goto(`https://www.moneycontrol.com/stocks/cptmarket/compsearchnew.php?search_str=${company_name}`);
-		if (reg.test(await page.url())) break;
+		if (reg.test(await page.url())) break; // implies, a specific company name had been searched , hence not showing choices
 		await page.waitForSelector('.srch_tbl');
 		var choices=await page.evaluate(
 			()=>[...document.querySelectorAll('.srch_tbl  tr td:first-child')]
