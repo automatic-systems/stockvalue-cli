df804cc9e2df04756ea4601432ce68f54ae2924e
Nikhilesh Yadav <nikhileshofficial@gmail.com>
2021-02-16 05:04:52 +0530

    +module: main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+const playwright = require('playwright');
+const choose_company = require('./choose.js');
+const quotes = require("./quote.js")
+
+async function main() {
+    const browser = await playwright.chromium.launch({headless:false});
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    while (true)
+    {
+        try{
+            await choose_company(page);
+            var quote=await quotes(page)
+            console.log()
+            console.table(quote);
+            await page.evaluate(() => window.stop());
+            await browser.close()
+            break
+        }
+        catch(e)
+        {
+            console.error("Error Occured!",e.msg)
+        }
+    }
+};
+main()
